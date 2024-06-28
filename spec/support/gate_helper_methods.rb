8feb433fe2abf2b685fc523d130bccd5b8f8a806
Jackson Barr <jackson.barr@simplybusiness.com>
2024-06-28 15:41:13 -0400

    Create gate_helper_methods

--- spec/support/gate_helper_methods.rb
+++ spec/support/gate_helper_methods.rb
@@ -1,8 +1,10 @@
 module GateHelperMethods
     def random_length_false_array(minimum_inputs, maximum_inputs)
+        # Returns array of false elements with a length in [minimum_inputs, maximum_inputs)
         Array.new(rand(minimum_inputs...maximum_inputs), false)
     end
     def random_length_true_array(minimum_inputs, maximum_inputs)
+        # Returns array of true elements with a length in [minimum_inputs, maximum_inputs)
         Array.new(rand(minimum_inputs...maximum_inputs), true)
     end
 end
