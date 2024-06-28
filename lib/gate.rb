b6270d66038ba075327fc600be538a6331b32f28
Jackson Barr <jackson.barr@simplybusiness.com>
2024-06-28 13:58:46 -0400

    Throw ArgumentError after instantiating Gate w/ less than num required inputs

--- lib/gate.rb
+++ lib/gate.rb
@@ -3,10 +3,17 @@ class Gate
 
     def initialize(*inputs)
         raise NotImplementedError if instance_of?(Gate)
+        if inputs.length < Gate.minimum_inputs
+            raise ArgumentError, "Expected at least #{Gate.minimum_inputs} inputs, got #{inputs.length}"
+        end
         @inputs = inputs
     end
 
     def output
         raise NotImplementedError
+    end
+
+    def self.minimum_inputs
+        2
     end
 end
