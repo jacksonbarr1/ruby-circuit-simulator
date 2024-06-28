4c760f136929d9dc3fee9dde7e953ccf3d6d9b49
Jackson Barr <jackson.barr@simplybusiness.com>
2024-06-28 11:34:19 -0400

    Add all input combinations for 2-input AndGate to Specfile

--- spec/and_gate_spec.rb
+++ spec/and_gate_spec.rb
@@ -6,9 +6,16 @@ RSpec.describe AndGate do
         and_gate.inputs = [0, 0]
         expect(and_gate.output).to eq(0)
     end
+    example "Input (0,1) returns 0" do
+        and_gate.inputs = [0, 1]
+        expect(and_gate.output).to eq(0)
+    end
+    example "Input (1,0) returns 0" do
+        and_gate.inputs = [1, 0]
+        expect(and_gate.output).to eq(0)
+    end
     example "Input (1,1) returns 1" do
         and_gate.inputs = [1, 1]
         expect(and_gate.output).to eq(1)
     end
-
 end
