5019989ae43ceb69ecd310c15f88afc474a54eeb
Jackson Barr <jackson.barr@simplybusiness.com>
2024-06-28 11:54:22 -0400

    Refactor: Add helper method to create AndGate

--- spec/and_gate_spec.rb
+++ spec/and_gate_spec.rb
@@ -1,21 +1,23 @@
 require_relative '../lib/and_gate'
 
 RSpec.describe AndGate do
-    let(:and_gate) { AndGate.new }
+    def create_and_gate_with_inputs(inputs)
+        AndGate.new(*inputs)
+    end
     example "Input (0,0) returns 0" do
-        and_gate.inputs = [0, 0]
+        and_gate = create_and_gate_with_inputs([0,0])
         expect(and_gate.output).to eq(0)
     end
     example "Input (0,1) returns 0" do
-        and_gate.inputs = [0, 1]
+        and_gate = create_and_gate_with_inputs([0,1])
         expect(and_gate.output).to eq(0)
     end
     example "Input (1,0) returns 0" do
-        and_gate.inputs = [1, 0]
+        and_gate = create_and_gate_with_inputs([1,0])
         expect(and_gate.output).to eq(0)
     end
     example "Input (1,1) returns 1" do
-        and_gate.inputs = [1, 1]
+        and_gate = create_and_gate_with_inputs([1,1])
         expect(and_gate.output).to eq(1)
     end
 end
