80a7217cdf28fd402214d808a95efbe30a109427
Jackson Barr <jackson.barr@simplybusiness.com>
2024-06-28 12:10:40 -0400

    Create shared example template to test Gate implementations

--- spec/and_gate_spec.rb
+++ spec/and_gate_spec.rb
@@ -1,23 +1,9 @@
 require_relative '../lib/and_gate'
+require_relative '../spec/gate_spec'
 
 RSpec.describe AndGate do
-    def create_and_gate_with_inputs(inputs)
-        AndGate.new(*inputs)
-    end
-    example "Input (0,0) returns 0" do
-        and_gate = create_and_gate_with_inputs([0,0])
-        expect(and_gate.output).to eq(0)
-    end
-    example "Input (0,1) returns 0" do
-        and_gate = create_and_gate_with_inputs([0,1])
-        expect(and_gate.output).to eq(0)
-    end
-    example "Input (1,0) returns 0" do
-        and_gate = create_and_gate_with_inputs([1,0])
-        expect(and_gate.output).to eq(0)
-    end
-    example "Input (1,1) returns 1" do
-        and_gate = create_and_gate_with_inputs([1,1])
-        expect(and_gate.output).to eq(1)
-    end
+    include_examples "a gate", AndGate, [0, 0], 0
+    include_examples "a gate", AndGate, [0, 1], 0
+    include_examples "a gate", AndGate, [1, 0], 0
+    include_examples "a gate", AndGate, [1, 1], 1
 end
--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -5,3 +5,10 @@ RSpec.describe Gate do
         expect { Gate.new }.to raise_error(NotImplementedError)
     end
 end
+
+RSpec.shared_examples "a gate" do |gate_class, inputs, expected_output|
+    it "returns #{expected_output} for inputs #{inputs}" do
+        gate = gate_class.new(*inputs)
+        expect(gate.output).to eq(expected_output)
+    end
+end
