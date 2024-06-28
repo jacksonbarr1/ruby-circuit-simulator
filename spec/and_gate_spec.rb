03cc39fee659573fef4223638e5f9b6e166d5bd7
Jackson Barr <jackson.barr@simplybusiness.com>
2024-06-28 15:40:56 -0400

    Spec: Test AndGate with random amount of inputs

--- spec/and_gate_spec.rb
+++ spec/and_gate_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/and_gate'
 require_relative './support/shared_examples'
 require_relative './support/shared_contexts'
+require_relative 'spec_helper'
 
 RSpec.describe AndGate do
     include_context "gate with insufficient inputs"
@@ -8,4 +9,10 @@ RSpec.describe AndGate do
     include_examples "a gate", AndGate, [false, true], false
     include_examples "a gate", AndGate, [true, false], false
     include_examples "a gate", AndGate, [true, true], true
+    it "returns false for a random-length set of inputs with exactly one false input" do
+        inputs = random_length_true_array(AndGate.minimum_inputs, AndGate.maximum_inputs)
+        inputs[rand(inputs.length)] = false
+        gate = AndGate.new(*inputs)
+        expect(gate.output).to eq(false)
+    end
 end
